4600605f4e84e9a917bbd1280bcf782db074499c
Aidan Cline <hermesappdev@gmail.com>
2019-08-23 00:42:06 -0500

    Final commit before rewrite

---
+++ rewrite/hermes/hermes/Tabs/Search.swift
@@ -0,0 +1,9 @@
+//
+//  Search.swift
+//  hermes
+//
+//  Created by Aidan Cline on 8/21/19.
+//  Copyright © 2019 Aidan Cline. All rights reserved.
+//
+
+import Foundation
---
+++ rewrite/hermes/hermes/Tabs/Subscriptions.swift
@@ -0,0 +1,9 @@
+//
+//  Subscriptions.swift
+//  hermes
+//
+//  Created by Aidan Cline on 8/21/19.
+//  Copyright © 2019 Aidan Cline. All rights reserved.
+//
+
+import Foundation
